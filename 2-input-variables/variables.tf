f3477eae9f8f163bcc8eee27fd1176bf4dd4a76a
Mihai <mihaimihai.prut+jd7fn@gmail.com>
2024-04-14 20:06:59 +0200

    Added default value to input variables Input variables

--- 2-input-variables/variables.tf
+++ 2-input-variables/variables.tf
@@ -1,9 +1,11 @@
 variable "policy_description" {
   type = string
   description = "Description for policy_string"
+  default = "Description for policy_string"
 }
 
 variable "policy_count" {
   type = number
   description = "Number of policies to create"
+  default = 0
 }
