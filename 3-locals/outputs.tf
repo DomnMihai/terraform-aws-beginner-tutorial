d8f465ed1486e4109e6bbd1d8cecbcfe1144a1cf
Mihai <mihaimihai.prut+ukd72@gmail.com>
2024-12-08 20:45:50 +0100

    Created sensitive output Outputs

--- 3-locals/outputs.tf
+++ 3-locals/outputs.tf
@@ -2,3 +2,9 @@ output "main_vpc_cidr" {
   value = aws_vpc.main.cidr_block
   description = "The CIDR block of main VPC"
 }
+
+output "policy_string_id" {
+  value = aws_iam_policy.policy_string.policy_id
+  description = "Policy ID of IAM Policy 'policy-string'"
+  sensitive = true
+}
