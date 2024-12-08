b7aa69ecc6a4d0ca3f316ab310fc893ec542a9fe
Mihai <mihaimihai.prut+ukd72@gmail.com>
2024-12-08 21:00:41 +0100

    Output with precondition Outputs

--- 3-locals/outputs.tf
+++ 3-locals/outputs.tf
@@ -8,3 +8,14 @@ output "policy_string_id" {
   description = "Policy ID of IAM Policy 'policy-string'"
   sensitive = true
 }
+
+output "main_subnets_ids" {
+  value = values(aws_subnet.main)[*].id
+  description = "List of subnets inside main VPC"
+
+  # All subnets' ids must start with -0
+  precondition {
+    condition     = length([ for key, value in aws_subnet.main : value.id if !startswith(value.id, "subnet-0") ]) == 0
+    error_message = "At least one subnet id doesn't start with 'subnet-0'"
+  }
+}
